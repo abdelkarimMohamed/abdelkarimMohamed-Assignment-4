788f0dcc2b714e4f60778f3abbf7cba22b35a337
Abdelkarim Mohamed <AbdoMohAli93@gmail.com>
2026-09-17 01:55:59 +0300

    Display All Sessions

---
+++ submission/assignment/AcademyScheduleAnalyzer/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace AcademyScheduleAnalyzer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sessionNames =
+        {
+            "C# Basics",
+            "Arrays",
+            "Functions",
+            "Date and Time",
+            "Exception Handling"
+        };
+
+        DateTime[] sessionDates =
+        {
+            new DateTime(2026, 9, 10, 18, 0, 0),
+            new DateTime(2026, 9, 13, 18, 0, 0),
+            new DateTime(2026, 9, 17, 18, 0, 0),
+            new DateTime(2026, 9, 20, 18, 0, 0),
+            new DateTime(2026, 9, 24, 18, 0, 0)
+        };
+
+        int[] sessionDurations =
+        {
+            180,
+            240,
+            180,
+            240,
+            180
+        };
+
+        DisplaySessions(sessionNames, sessionDates, sessionDurations);
+
+
+    }
+    static void DisplaySessions(string[] names, DateTime[] dates, int[] durations)
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {names[i]}");
+            Console.WriteLine($"Date: {dates[i].ToString("dd MMMM yyyy")}");
+            Console.WriteLine($"Start Time: {dates[i].ToString("hh:mm tt")}");
+            Console.WriteLine($"Duration: {durations[i]} minutes");
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
